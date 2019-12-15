cf9da436eeffbb47af619fd4687f6c90dac2594d
Richard Del Toro <richarddeltorovs@gmail.com>
2019-12-14 23:43:38 -0700

    finised dj css

--- exercises/djcss-Css-transiton/app.js
+++ exercises/djcss-Css-transiton/app.js
@@ -8,10 +8,147 @@ grab.addEventListener("click", runIt)
 /////DRUM 1 
 
 
+//CLAP 1
 function runIt2(){
     grab2.play();
 }
 
-///DRUM2 
+///CLAP 1
 let grab2 = document.getElementById("audioDrum2")
 grab2.addEventListener("click", runIt2)
+
+
+//CHIM 1
+function runIt3(){
+    grab3.play();
+}
+
+//CHIM 1
+let grab3 = document.getElementById("audiocym1")
+grab3.addEventListener("click",runIt3)
+
+//Bass drum
+function runIt4(){
+    grab4.play();
+}
+
+let grab4 = document.getElementById("audioBass1")
+grab4.addEventListener("click", runIt4)
+//Bass Drum
+
+//DOG BARK
+function runIt5(){
+    grab5.play();
+}
+
+
+let grab5 = document.getElementById("DogBark1")
+grab5.addEventListener("click",runIt5)
+//DOG BARK
+
+
+//Broken Glass
+function runIt6(){
+    grab6.play();
+}
+
+let grab6 = document.getElementById("glass")
+grab6.addEventListener("click",runIt6)
+//Broken Glass
+
+
+
+//WOMEN SCREAM
+function runIt7(){
+    grab7.play();
+}
+
+let grab7 = document.getElementById("WomanScrem")
+grab7.addEventListener("click",runIt7)
+// WOMEN SCREAM
+
+
+
+
+// DJ SCRATH
+function runIt8(){
+    grab8.play();
+}
+
+let grab8 = document.getElementById("djscrath")
+grab8.addEventListener("click", runIt8)
+// DJ SRATCH
+
+
+// Bomb
+function runIt9(){
+    grab9.play();
+}
+
+let grab9 = document.getElementById("Bomb")
+grab9.addEventListener("click",runIt9)
+// Bomb
+
+
+
+
+// Guitar
+function runIt10(){
+    grab10.play();
+}
+
+let grab10 = document.getElementById("guitar")
+grab10.addEventListener("click",runIt10)
+// Guitar
+
+
+
+// Bell
+function runIt11(){
+    grab11.play();
+}
+
+let grab11 = document.getElementById("Bell")
+grab11.addEventListener("click",runIt11)
+// Bell
+
+
+
+// Man Scream
+function runIt12(){
+    grab12.play();
+}
+
+let grab12 = document.getElementById("manScrem")
+grab12.addEventListener("click",runIt12)
+// Man Scearm
+
+
+
+// Car Sound
+function runIt13(){
+    grab13.play();
+}
+
+let grab13 = document.getElementById("carsound")
+grab13.addEventListener("click",runIt13)
+// Car Sound
+
+
+// Alarm Sound
+function runIt14(){
+    grab14.play();
+}
+
+let grab14 = document.getElementById("alarm")
+grab14.addEventListener("click", runIt14)
+// Alarm Sound
+
+
+// THUNDER
+function runIt15(){
+    grab15.play();
+}
+
+let grab15 = document.getElementById("thunder")
+grab15.addEventListener("click",runIt15)
